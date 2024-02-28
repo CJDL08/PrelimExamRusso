681bf5bc3489e23e1fd219ebe6453e9ab4c858c1
CJDL08 <russo.johncyril@cec.edu.ph>
2024-02-28 22:50:29 +0800

    5th version

---
+++ assets/pages/booking.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const bookingForm = document.getElementById('bookingForm');
+
+    bookingForm.addEventListener('submit', function (event) {
+        event.preventDefault();        
+        alert('Thank you for booking with us, Please check your email!');
+        window.location.href = '../../main.html';
+
+
+    });
+});
